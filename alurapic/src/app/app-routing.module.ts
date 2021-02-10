05abc860285ee5553dccc88c2c932ccebff208dc
Jackson Luís Peres das Neves <jackson.luis@jv01.local>
2021-02-10 14:14:06 -0300

    created pipe and added resolver

--- alurapic/src/app/app-routing.module.ts
+++ alurapic/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from "@angular/router";
 import { NotFoundComponent } from "./errors/not-found/not-found.component";
 import { PhotoFormComponent } from "./photos/photo-form/photo-form.component";
 import { PhotoListComponent } from "./photos/photo-list/photo-list.component";
+import { PhotoListResolver } from "./photos/photo-list/photo-list.resolver";
 
 const routes: Routes = [
     {
         path: 'user/:userName',
-        component: PhotoListComponent
+        component: PhotoListComponent,
+        resolve: {
+            photos: PhotoListResolver
+        }
     },
     {
         path: 'p/add',
