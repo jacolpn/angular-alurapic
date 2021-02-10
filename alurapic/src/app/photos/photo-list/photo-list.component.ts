3174458be6e317bc286922d33cfaa823ed9eac9a
Jackson Luís Peres das Neves <jackson.luis@jv01.local>
2021-02-10 17:09:54 -0300

    created search component

--- alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
@@ -10,10 +8,10 @@ import { PhotoService } from '../photo/photo.service';
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html'
 })
-export class PhotoListComponent implements OnInit, OnDestroy {
+export class PhotoListComponent implements OnInit {
   photos: Photo[] = [];
   filter: string = '';
-  debounce: Subject<string> = new Subject<string>();
+  
   hasMore: boolean = true;
   currentPage: number = 1;
   userName: string = '';
@@ -26,19 +24,13 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data['photos'];
-    this.debounce
-      .pipe(debounceTime(300))
-      .subscribe(filter => this.filter = filter);
-  }
-
-  ngOnDestroy(): void {
-    this.debounce.unsubscribe();
   }
 
   load() {
     this.photoService
       .listFromUserPaginated(this.userName, ++this.currentPage)
       .subscribe(photos => {
+        this.filter = '';
         this.photos = this.photos.concat(photos);
 
         if (!photos.length) {
