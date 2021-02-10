bbe12b4857179d88b77283e992c31d8e368e7dcb
Jackson Luís Peres das Neves <jackson.luis@jv01.local>
2021-02-10 16:46:25 -0300

    created card component

--- alurapic/src/app/photos/photos.module.ts
+++ alurapic/src/app/photos/photos.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
 
 import { PhotoModule } from './photo/photo.module';
 import { PhotoListModule } from './photo-list/photo-list.module';
