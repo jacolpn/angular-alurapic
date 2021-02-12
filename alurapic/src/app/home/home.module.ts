7c52aac385f054de217901b7f88682190c4b274c
Jackson Luís Peres das Neves <jackson.luis@jv01.local>
2021-02-12 17:06:26 -0300

    created login and configured route

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -4,13 +4,15 @@ import { SignInComponent } from "./signin/signin.component";
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from "@angular/common";
 import { VMessageModule } from "../shared/components/vmessage/vmessage.module";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
     declarations: [SignInComponent],
     imports: [
         ReactiveFormsModule,
         CommonModule,
-        VMessageModule
+        VMessageModule,
+        RouterModule
     ]
 })
 export class HomeModule {}
