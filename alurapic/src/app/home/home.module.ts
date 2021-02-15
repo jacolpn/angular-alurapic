c6b5cc444d48635448fd2463f4f0e61e3c76ddb7
Jackson Luís Peres das Neves <jackson.luis@jv01.local>
2021-02-14 21:17:11 -0300

    created home(img) component and added route

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -6,11 +6,13 @@ import { RouterModule } from "@angular/router";
 import { SignInComponent } from "./signin/signin.component";
 import { VMessageModule } from "../shared/components/vmessage/vmessage.module";
 import { SignUpComponent } from "./signup/signup.component";
+import { HomeComponent } from "./home.component";
 
 @NgModule({
     declarations: [
         SignInComponent,
-        SignUpComponent
+        SignUpComponent,
+        HomeComponent
     ],
     imports: [
         ReactiveFormsModule,
