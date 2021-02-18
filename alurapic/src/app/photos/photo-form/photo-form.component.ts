ae5fda495aa9e8aebdffe9e14defef0ecea2c5f8
jacolpn <jacolpn@outlook.com>
2021-02-18 08:48:13 -0300

    created photo form and implemented validation

--- alurapic/src/app/photos/photo-form/photo-form.component.ts
+++ alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-photo-form',
   templateUrl: './photo-form.component.html'
 })
 export class PhotoFormComponent implements OnInit {
+  photoForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.photoForm = this.formBuilder.group({
+      file: ['', Validators.required],
+      description: ['', Validators.maxLength(100)],
+      allowComments: [true]
+    });
   }
 
 }
--- alurapic/src/app/photos/photo-form/photo-form.module.ts
+++ alurapic/src/app/photos/photo-form/photo-form.module.ts
@@ -1,10 +1,17 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { VMessageModule } from "src/app/shared/components/vmessage/vmessage.module";
 import { PhotoFormComponent } from "./photo-form.component";
 
 @NgModule({
     declarations: [PhotoFormComponent],
-    imports: [CommonModule]
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        VMessageModule,
+        FormsModule
+    ]
 })
 export class PhotoFormModule {
 
